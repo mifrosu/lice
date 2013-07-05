97d1a93d440cdc090eb303a779ab1b6d24615f7e
Michael O'Sullivan <mifrosu@gmail.com>
2013-07-05 09:06:57 +0100

    Stored lines are now left-aligned and padded with spaces (processLine) The addLine method chomps terminal white space. attr_reader set for storage array. Unit tests are updated and refactored.

--- lice.rb
+++ lice.rb
@@ -3,8 +3,14 @@
 module Lice
     
     class LiceParser
+        # The MAX_WIDTH should be 79 chars. We will allow 4 chars for a single
+        # char comment character and a space at both ends of the line.
+        # We will set MAX_TEXT_WIDTH to be 75.
 
         MAX_WIDTH = 79
+        MAX_TEXT_WIDTH = 75
+
+        attr_reader :liceArray
         
         def initialize
             @liceArray = Array.new
@@ -12,22 +18,29 @@ module Lice
         end
 
         def LiceParser.checkLine line
-            if line.size > MAX_WIDTH
+            if line.size > MAX_TEXT_WIDTH
                 false
             else
                 true
             end
         end
 
+        def processLine line
+            # left justify and pad line with spaces
+            return line.ljust(MAX_TEXT_WIDTH)
+        end
+
         def addLine line
+            line.chomp!
             if !@cacheArray.empty?
                 line = @cacheArray[0] + " " + line
                 @cacheArray.clear 
+            end
             if LiceParser.checkLine(line) 
-                @liceArray.push line
+                @liceArray.push processLine(line) 
             else
-                breakIndex = line.rindex(/\s/,MAX_WIDTH-1)
-                @liceArray.push line[0..breakIndex-1]
+                breakIndex = line.rindex(/\s/,MAX_TEXT_WIDTH-1)
+                @liceArray.push processLine(line[0..breakIndex-1])
                 @cacheArray.push line[breakIndex+1..-1]
             end
         end
--- tests/tc_lice.rb
+++ tests/tc_lice.rb
@@ -4,27 +4,29 @@ require "test/unit"
 class TestLineLength < Test::Unit::TestCase
 
     def setup
+        @testLice = Lice::LiceParser.new
         @line="giraffe giraffe giraffe giraffe giraffe giraffe"\
               " giraffe giraffe giraffe giraffe giraffe"  # 87 chars
+        @shortLine = "this is a short line\n"
     end
 
     def test_checkLine_short
-        assert(Lice::LiceParser.checkLine("this is a short line"))
+        assert(Lice::LiceParser.checkLine(@shortLine))
     end
 
     def test_checkLine_long
         assert(!Lice::LiceParser.checkLine(@line))
     end
 
-    def test_liceParser_addLine
-        testLice = Lice::LiceParser.new
-        testLice.addLine "this is a short line"
-        assert_equal(testLice.size, [1, 0])
+    def test_LiceParser_addLine
+        @testLice.addLine @shortLine 
+        assert_equal(@testLice.size, [1, 0])
+        assert_equal(@testLice.liceArray[0],
+                     "this is a short line".ljust(75))
     end
 
-    def test_liceParser_addLine_long
-        testLice = Lice::LiceParser.new
-        testLice.addLine @line
-        assert_equal(testLice.size, [1,1])
+    def test_LiceParser_addLine_long
+        @testLice.addLine @line
+        assert_equal(@testLice.size, [1,1])
     end
 end
