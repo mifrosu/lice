9cc8841eccf1f7f6ab5ffede596d095f081c6f36
Michael O'Sullivan <mifrosu@gmail.com>
2013-07-07 20:33:43 +0100

    Further implementation of LiceProcess library with unit tests. Files are identified by file name suffix, or hash bang line if present. Adds a test script with a hash bang comment for unit testing.

--- lib/lice/lice_process.rb
+++ lib/lice/lice_process.rb
@@ -1,7 +1,42 @@
 #!/usr/bin/env ruby
 
+require_relative "./lice_parser"
+
 module Lice
     class LiceProcess
+
+        @@LANG_COMMENTS = {
+            :c_style => ["/**", "*", "*/"],
+            :cpp_style => ["//"],
+            :script_style => ["#"],
+            :vim_style => ["\""], 
+            :haskell => ["{-", " ", "-}"],
+            :percent => ["%"]         # Erlang, TeX
+        }
+
+        @@SUFFIX_ID = {
+            "c" => :c_style,
+            "h" => :c_style,
+            "cpp" => :c_style,
+            "cxx" => :c_style,
+            "hpp" => :c_style,
+            "cs" => :c_style,   # C#
+            "java" => :c_style,
+            "class" => :c_style,
+            "jar" => :c_style,
+            "rb" => :script_style,
+            "py" => :script_style,
+            "pl" => :script_style,  # perl
+            "pm" => :script_style,
+            "t" => :script_style,
+            "pod" => :script_style,
+            "vim" => :vim_style,
+            "tex" => :percent,
+            "erl" => :percent,
+            "hrl" => :percent,
+            "hs" => :haskell,
+            "lhs" => :haskell
+        }
 
         attr_reader :licenceFileName
         attr_reader :sourceFileArray
@@ -11,7 +46,40 @@ module Lice
             @licenceFileName = @sourceFileArray.shift
         end
 
-        def indentify fileName
+        def LiceProcess.getSuffix fileName
+            if fileName.match(/\./)
+                return fileName.split(/\./)[-1]
+            else
+                return nil
+            end
         end
+
+        def LiceProcess.identifyFileType fileName
+            fileSuffix = getSuffix fileName
+            if fileSuffix
+                if @@SUFFIX_ID.has_key? fileSuffix
+                   return @@SUFFIX_ID.fetch fileSuffix
+                end
+            end 
+        end
+
+        def LiceProcess.getComment fileName
+            fileType = LiceProcess.identifyFileType fileName
+            if fileType 
+                return @@LANG_COMMENTS.fetch fileType
+            else
+                if File.exist? fileName
+                    firstLine = nil
+                    File.open fileName do |file|
+                        firstLine = file.gets
+                    end
+                    # test if first line begins "#!" e.g. #!/usr/bin/env ruby
+                    if firstLine.match /^\#!/
+                        return @@LANG_COMMENTS.fetch :script_style
+                    end
+                end
+            end
+        end
+
     end
 end
--- test/tc_lice_process.rb
+++ test/tc_lice_process.rb
@@ -6,11 +6,14 @@ require_relative "../lib/lice/lice_process"
 
 class TestLiceProcess < Test::Unit::TestCase
 
+        
+
     context "reading fileNameArray" do
 
         setup do
             @fileNameArray = ["licence.txt", "src1.rb", "src2.py", "src3.sh"]
             @testProcess = Lice::LiceProcess.new(@fileNameArray)
+            
         end
         should "identify item 0 as licence file" do
             assert_equal("licence.txt", @testProcess.licenceFileName)
@@ -30,14 +33,90 @@ class TestLiceProcess < Test::Unit::TestCase
     end
 
     context "processing source files" do
+
+        
         should "check file exists" do
         end
 
+        should "get file suffix" do
+            assert_equal("rb", Lice::LiceProcess.getSuffix("test.rb"))
+            assert_equal(nil, Lice::LiceProcess.getSuffix("test_script"))
+        end
+
         should "identify file type" do
+            assert_equal(:c_style, 
+                         Lice::LiceProcess.identifyFileType("src.cpp"))
+        end
+
+        should "get required comment" do
+            assert_equal(["#"], Lice::LiceProcess.getComment("src.rb"))
         end
 
         should "select the correct comment type" do
+            testFileNameArray = ["source.c", "header.h", "source.cpp",
+            "src.cxx", "header.hpp", "sharp.cs", "java.java",
+            "java.class", "java.jar", "script.rb", "script.py", "perl.pl",
+            "perl.pm", "perl.t", "perl.pod", "ruby-vim.vim", "thesis.tex",
+            "erlang.erl", "erlang_header.hrl", "haskell.hs", "hask.lhs"]
+            commentHash = {
+                :c_style => ["/**", "*", "*/"],
+                :cpp_style => ["//"],
+                :script_style => ["#"],
+                :vim_style => ["\""], 
+                :haskell => ["{-", " ", "-}"],
+                :percent => ["%"]         # Erlang, TeX
+            }
+            expectedCommentArray = [
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:c_style],
+                commentHash[:script_style],
+                commentHash[:script_style],
+                commentHash[:script_style],
+                commentHash[:script_style],
+                commentHash[:script_style],
+                commentHash[:script_style],
+                commentHash[:vim_style],
+                commentHash[:percent],
+                commentHash[:percent],
+                commentHash[:percent],
+                commentHash[:haskell],
+                commentHash[:haskell]
+            ]
+            for num in (0..testFileNameArray.size-1)
+               assert_equal(expectedCommentArray[num],
+                           Lice::LiceProcess.getComment(testFileNameArray[num])) 
+            end
         end
+
+        should "identify file from hash bang line if no file name suffix" do
+           # This test may only be run from lice/test/ or lice/
+            testScript = nil 
+            if Dir.pwd.match /lice$/
+               testScript = Dir.pwd + "/test/test_script" 
+            elsif Dir.pwd.match /lice\/test$/
+                testScript = "test_script"
+            end
+            if testScript 
+                assert_equal(["#"], Lice::LiceProcess.getComment(testScript),
+                        "Check working directory is lice/ or lice/test/")
+            else
+                puts
+                puts
+                puts "*************************************************"
+                puts "* Note: hash bang test has not been run!        *"
+                puts "* The test must be run from lice/ or lice/test/ *"
+                puts "*************************************************"
+                puts
+            end
+        end
+
 
         should "write out processed licence text to file" do
         end
